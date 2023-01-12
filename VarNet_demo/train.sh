5a043fc64fa7c50ddc845e8f09ef88e78c18229d
Breeze <78247190+Breeze-Zero@users.noreply.github.com>
2023-01-12 14:30:21 +0800

    Update train.sh

--- VarNet_demo/train.sh
+++ VarNet_demo/train.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=2,3 python train_varnet_demo.py \
     --challenge multicoil \
-    --data_path /data2/fastmri \
+    --data_path data_root_path \
     --center_fractions 0.1171875 0.1171875 \
     --accelerations 2 3 \
     --mask_type equispaced_fraction \
